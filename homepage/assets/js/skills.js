deb819712a892e59b02d20b5306f9597267b464c
Najila Joanne Batistil <najilabatistil1@gmail.com>
2025-04-29 23:49:16 +0800

    [Home Page] Refactor code

--- homepage/assets/js/skills.js
+++ homepage/assets/js/skills.js
@@ -5,27 +5,27 @@ var skills = [
       {
         name: "PHP",
         image: "php.png",
-        rating: 3.5
+        rating: 3
       },
       {
         name: "SQL",
         image: "sql.png",
-        rating: 3.5
+        rating: 3
       },
       {
         name: "JavaScript",
         image: "javascript.png",
-        rating: 3
+        rating: 2.5
       },
       {
         name: "Java",
         image: "java.png",
-        rating: 2.5
+        rating: 2
       },
       {
         name: "Python",
         image: "python.png",
-        rating: 2
+        rating: 1.5
       }
     ]
   },
@@ -75,16 +75,32 @@ var skills = [
       {
         name: "Figma",
         image: "figma.png",
-        rating: 3
+        rating: 2.5
       },
       {
         name: "Photoshop",
         image: "photoshop.png",
-        rating: 2.5
+        rating: 2
       }
     ]
   }
 ]
+
+function createSkillsSection() {
+  var skillsContainer = document.getElementById("skillsContainer");
+
+  skills.forEach(skill => {
+    skillsContainer.innerHTML +=
+      `
+      <div class="row pb-2">
+        <div class="col-12 p-0">
+          <h2 class="text-center text-md-start">`+ skill.category + `</h2>
+        </div>
+        `+ createSkillCards(skill.cards) + `
+      </div>
+      `;
+  });
+}
 
 function createStars(rating) {
   var stars = '';
@@ -102,45 +118,30 @@ function createStars(rating) {
   return stars;
 }
 
-function createSkillCards() {
-  for (var i = 0; i < skills.length; i++) {
-    var skillsContainer = document.getElementById("skillsContainer");
+function createSkillCards(skillCards) {
+  var skillList = '';
 
-    var skill = skills[i];
-    var skillList = '';
+  skillCards.forEach(skillCard => {
+    skillList +=
+    `
+    <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center px-1 px-lg-2 my-3">
+        <div class="card p-3 h-100">
+          <!-- Image -->
+          <div class="skill-img-container d-flex justify-content-center align-items-center p-3">
+            <img src="homepage/assets/img/skills/`+ skillCard.image + `" class="skill-img" alt="` + skillCard.name + ` Logo">
+          </div>
 
-    for (var j = 0; j < skill.cards.length; j++) {
-      var skillCard = skill.cards[j];
-
-      skillList +=
-        `
-        <div class="col-6 col-md-4 col-lg-3 d-flex justify-content-center px-1 px-lg-2 my-3">
-          <div class="card p-3 h-100">
-            <!-- Image -->
-            <div class="skill-img-container d-flex justify-content-center align-items-center p-3">
-              <img src="homepage/assets/img/skills/`+ skill.cards[j].image + `" class="skill-img" alt="` + skill.cards[j].name + ` Logo">
-            </div>
-
-            <!-- Body -->
-            <div class="card-body d-flex flex-column align-items-center text-center pt-2 px-0 pb-0 my-auto">
-                <h4 class="card-title">`+ skill.cards[j].name + `</h2>
-                <p class="card-text">`+ createStars(skill.cards[j].rating) + `</p>
-            </div>
+          <!-- Body -->
+          <div class="card-body d-flex flex-column align-items-center text-center pt-2 px-0 pb-0 my-auto">
+              <h4 class="card-title">`+ skillCard.name + `</h4>
+              <p class="card-text">`+ createStars(skillCard.rating) + `</p>
           </div>
         </div>
-      `;
-    }
-
-    skillsContainer.innerHTML +=
-      `
-      <div class="row pb-2">
-        <div class="col-12 p-0">
-          <h2 class="text-center text-md-start">`+ skill.category + `</h2>
-        </div>
-        `+ skillList + `
       </div>
-      `;
-  }
+    `;
+  });
+
+  return skillList;
 }
 
-createSkillCards();
+createSkillsSection();
